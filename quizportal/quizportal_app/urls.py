9939a4189df0c1ff3da0f1480f6801b7daf85aa9
Varsha Verma <varsha.verma.eee15@itbhu.ac.in>
2016-07-17 18:01:10 +0530

    Normal Login and Signup

---
+++ quizportal/quizportal_app/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^signup/$', views.register, name='signup'),
+    url(r'^login/$', views.user_login, name='login'),
+    url(r'^logout/$', views.user_logout, name='logout'),
+]
